599ccada76a7a592b9b1ffec302ac391aa91c477
aleksei.dudko <aleksei.dudko@gmail.com>
2014-06-11 23:49:30 +0300

    Initial project was created.

---
+++ highwave/src/com/dudko/highwave/HighwaveServlet.java
@@ -0,0 +1,13 @@
+package com.dudko.highwave;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HighwaveServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
