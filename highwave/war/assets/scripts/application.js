81235b9b1ed54790dbe08c76dd0978f6d70ae4bc
aleksei.dudko <aleksei.dudko@gmail.com>
2014-06-25 23:19:16 +0300

    Initial support of Deposit was implemented.

--- highwave/war/assets/scripts/application.js
+++ highwave/war/assets/scripts/application.js
@@ -1,13 +1,20 @@
 MyApp = new Backbone.Marionette.Application();
 
 MyApp.addRegions({
-	generatorRegion : "#generator"
+	generatorRegion : "#generator",
+	resultsRegion : "#results"
 });
 
 Bank = Backbone.Model.extend({});
 
+Deposit = Backbone.Model.extend({});
+
 Banks = Backbone.Collection.extend({
 	model : Bank
+});
+
+Deposits = Backbone.Collection.extend({
+	model : Deposit
 });
 
 BankView = Backbone.Marionette.ItemView.extend({
@@ -16,13 +23,30 @@ BankView = Backbone.Marionette.ItemView.extend({
 	className : "bank"
 });
 
+DepositView = Backbone.Marionette.ItemView.extend({
+	template : "#deposit-template",
+	tagName : "tr",
+	className : "deposit"
+});
+
 BanksView = Backbone.Marionette.CompositeView.extend({
 	tagName : "table",
 	id : "banks",
 	template : "#banks-template",
-	itemView : BankView,
+	childView : BankView,
 
-	appendHtml : function(collectionView, itemView) {
+	attachHtml : function(collectionView, itemView) {
+		collectionView.$("tbody").append(itemView.el);
+	}
+});
+
+DepositsView = Backbone.Marionette.CompositeView.extend({
+	tagName : "table",
+	id : "deposits",
+	template : "#deposits-template",
+	childView : DepositView,
+
+	attachHtml : function(collectionView, itemView) {
 		collectionView.$("tbody").append(itemView.el);
 	}
 });
@@ -31,20 +55,29 @@ MyApp.addInitializer(function(options) {
 	var banksView = new BanksView({
 		collection : options.banks
 	});
+	var depositsView = new DepositsView({
+		collection : options.deposits
+	});
+
 	MyApp.generatorRegion.show(banksView);
+	MyApp.resultsRegion.show(depositsView);
 });
 
 $(document).ready(function() {
 	var banks = new Banks([ new Bank({
 		name : "BelarusBank"
-	}), new Bank({
-		name : "Empty"
+	}) ]);
+
+	var deposits = new Deposits([ new Deposit({
+		name : "Wave",
+		interest : "45"
+	}), new Deposit({
+		name : "7",
+		interest : "45+"
 	}) ]);
 
 	MyApp.start({
-		banks : banks
+		banks : banks,
+		deposits : deposits
 	});
-
-	banks.add();
-	banks.add();
 });
