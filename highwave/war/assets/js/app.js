05cae0be17b33abd97b1b126c05d40943301f6ab
aleksei.dudko <aleksei.dudko@gmail.com>
2014-07-06 17:35:13 +0300

    Bootstrapper style was applied.

--- highwave/war/assets/js/app.js
+++ highwave/war/assets/js/app.js
@@ -1,35 +1,33 @@
 MyApp = new Backbone.Marionette.Application();
 
-var ModalRegion = Backbone.Marionette.Region
-		.extend({
-			el : "#modal",
+var ModalRegion = Backbone.Marionette.Region.extend({
+    el : "#modal",
 
-			constructor : function() {
-				_.bindAll(this, "showModal", "hideModal");
-				Backbone.Marionette.Region.prototype.constructor.apply(this,
-						arguments);
-				this.on("show", this.showModal, this);
-			},
+    constructor : function() {
+	    _.bindAll(this, "showModal", "hideModal");
+	    Backbone.Marionette.Region.prototype.constructor.apply(this, arguments);
+	    this.on("show", this.showModal, this);
+    },
 
-			getEl : function(selector) {
-				var $el = $(selector);
-				$el.on("hidden", this.close);
-				return $el;
-			},
+    getEl : function(selector) {
+	    var $el = $(selector);
+	    $el.on("hidden", this.close);
+	    return $el;
+    },
 
-			showModal : function(view) {
-				view.on("close", this.hideModal, this);
-				this.$el.modal("show");
-			},
+    showModal : function(view) {
+	    view.on("close", this.hideModal, this);
+	    this.$el.modal("show");
+    },
 
-			hideModal : function() {
-				this.$el.modal("hide");
-			}
-		});
+    hideModal : function() {
+	    this.$el.modal("hide");
+    }
+});
 
 MyApp.addRegions({
-	content : "#content",
-	modal : ModalRegion
+    content : "#results",
+    modal : ModalRegion
 });
 
 $(document).ready(function() {
