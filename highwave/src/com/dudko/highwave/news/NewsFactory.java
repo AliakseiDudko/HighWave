2fb19ec24d8ee67bc464e1b64215deacfc460921
aleksei.dudko <aleksei.dudko@gmail.com>
2014-07-15 23:00:51 +0300

    Requests to the Twitte API is limited. The latest request is cached.

--- highwave/src/com/dudko/highwave/news/NewsFactory.java
+++ highwave/src/com/dudko/highwave/news/NewsFactory.java
@@ -3,6 +3,9 @@ package com.dudko.highwave.news;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.joda.time.DateTime;
+import org.joda.time.Seconds;
+
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -10,21 +13,31 @@ import twitter4j.TwitterFactory;
 
 public class NewsFactory {
 	private static final Twitter twitter;
+	private static List<Status> newsFeed;
+
+	private static final Seconds generationInterval;
+	private static DateTime lastTimeGeneration;
 
 	static {
 		twitter = new TwitterFactory().getInstance();
+		newsFeed = new ArrayList<Status>();
+
+		int seconds = 60;
+		generationInterval = Seconds.seconds(seconds);
+		lastTimeGeneration = DateTime.now().minusSeconds(seconds);
 	}
 
 	public List<Status> getNewsFeed() {
-
-		List<Status> statuses;
-		try {
-			statuses = twitter.getHomeTimeline();
-		} catch (TwitterException e) {
-			statuses = new ArrayList<Status>();
-			e.printStackTrace();
+		Seconds interval = Seconds.secondsBetween(lastTimeGeneration, DateTime.now());
+		if (interval.isGreaterThan(generationInterval)) {
+			try {
+				newsFeed = twitter.getHomeTimeline();
+				lastTimeGeneration = DateTime.now();
+			} catch (TwitterException e) {
+				e.printStackTrace();
+			}
 		}
 
-		return statuses;
+		return newsFeed;
 	}
 }
