c2d630f1736b7817c4775f5de27741371551eb45
aleksei.dudko <aleksei.dudko@gmail.com>
2014-07-16 23:52:12 +0300

    News feed includes only top 5 tweets.

--- highwave/src/com/dudko/highwave/news/NewsFactory.java
+++ highwave/src/com/dudko/highwave/news/NewsFactory.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.joda.time.DateTime;
 import org.joda.time.Seconds;
 
+import twitter4j.Paging;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -31,7 +32,8 @@ public class NewsFactory {
 		Seconds interval = Seconds.secondsBetween(lastTimeGeneration, DateTime.now());
 		if (interval.isGreaterThan(generationInterval)) {
 			try {
-				newsFeed = twitter.getHomeTimeline();
+				Paging paging = new Paging(1, 5);
+				newsFeed = twitter.getHomeTimeline(paging);
 				lastTimeGeneration = DateTime.now();
 			} catch (TwitterException e) {
 				e.printStackTrace();
