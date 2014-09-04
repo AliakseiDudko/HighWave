3f8f3e128e263267b3520a91a435322e1bc01b7f
aleksei.dudko <aleksei.dudko@gmail.com>
2014-09-04 23:15:05 +0300

    MTSquirrels deposit was refactored.

--- highwave/src/com/dudko/highwave/deposit/deposits/MTSquirrels.java
+++ highwave/src/com/dudko/highwave/deposit/deposits/MTSquirrels.java
@@ -11,17 +11,15 @@ import com.dudko.highwave.deposit.*;
 public class MTSquirrels extends Deposit {
 	private float minOpenAmount = 500000f;
 	private float lowInterestRate = 1.0f;
-	private int fixPeriodMonths;
+	private int fixPeriodMonths = 2;
 
-	public MTSquirrels(int id, String name, float interestRate, int fixPeriodMonths) {
+	public MTSquirrels() {
+		id = 6;
 		bank = BankFactory.GetBank(BankCode.MTBank);
+		name = "МТБелки 18-2";
 		url = "http://www.mtbank.by/private/deposits/br/mtbelki/";
 		currency = Currency.BYR;
-
-		this.id = id;
-		this.name = name;
-		this.interestRate = interestRate;
-		this.fixPeriodMonths = fixPeriodMonths;
+		interestRate = 30.0f;
 	}
 
 	@Override
@@ -30,45 +28,51 @@ public class MTSquirrels extends Deposit {
 			return null;
 		}
 
-		DateTime currentDate = DateTime.now();
-		DateTime maxEndDate = DateTime.now().plusMonths(18);
-
-		period = Math.min(Days.daysBetween(currentDate, maxEndDate).getDays(), period);
-		DateTime endDate = currentDate.plusDays(period);
-
 		List<AccountStatementRecord> list = new ArrayList<AccountStatementRecord>();
 
+		DateTime currentDate = DateTime.now();
+		int term = Math.min(Days.daysBetween(currentDate, currentDate.plusMonths(18)).getDays(), period);
+		DateTime endDate = currentDate.plusDays(term);
 		float depositAmount = amount;
 
-		AccountStatementRecord record = new AccountStatementRecord(currentDate, depositAmount, interestRate, "Открытие вклада.");
-		list.add(record);
+		addRecord(list, currentDate, depositAmount, interestRate, "Открытие вклада.");
 
 		DateTime previousDate = currentDate;
 		DateTime fixPeriodDate = currentDate.plusMonths(fixPeriodMonths);
 		currentDate = currentDate.plusMonths(1);
 
-		while (fixPeriodDate.isBefore(endDate) || fixPeriodDate.isEqual(endDate)) {
-			for (int i = 0; i < fixPeriodMonths; i++) {
+		int months = 0;
+		while (currentDate.isBefore(endDate) || currentDate.isEqual(endDate)) {
+			float _interestRate = fixPeriodDate.isBefore(endDate) || fixPeriodDate.isEqual(endDate) ? interestRate : lowInterestRate;
+			for (int i = 0; i < fixPeriodMonths && currentDate.isBefore(endDate) || currentDate.isEqual(endDate); i++) {
 				int _period = Days.daysBetween(previousDate, currentDate).getDays();
-				depositAmount = calculatePeriod(depositAmount, interestRate, _period);
-				record = new AccountStatementRecord(currentDate, depositAmount, interestRate, "Капитализация.");
-				list.add(record);
+				depositAmount = calculatePeriod(depositAmount, _interestRate, _period);
+				addRecord(list, currentDate, depositAmount, _interestRate, "Капитализация.");
 
+				months++;
 				previousDate = currentDate;
 				currentDate = currentDate.plusMonths(1);
+			}
+
+			int _period = Days.daysBetween(previousDate, endDate).getDays();
+			if (_period > 0 && months != 0 && months % fixPeriodMonths == 0) {
+				depositAmount *= 1.005f;
+				addRecord(list, currentDate, depositAmount, interestRate, "Бонус 0,5%.");
 			}
 
 			fixPeriodDate = fixPeriodDate.plusMonths(fixPeriodMonths);
 		}
 
 		int _period = Days.daysBetween(previousDate, endDate).getDays();
-		if (_period == 0) {
-			record = new AccountStatementRecord(endDate, depositAmount, interestRate, "Закрытие вклада.").setIsLast(true);
-			list.add(record);
-		} else {
+		if (_period != 0) {
 			depositAmount = calculatePeriod(depositAmount, lowInterestRate, _period);
-			record = new AccountStatementRecord(endDate, depositAmount, lowInterestRate, "Досрочное закрытие вклада.").setIsLast(true);
-			list.add(record);
+			addRecord(list, endDate, depositAmount, lowInterestRate, "Начисление процентов.");
+		}
+
+		if (months % fixPeriodMonths == 0 && _period == 0) {
+			addRecord(list, endDate, depositAmount, interestRate, "Закрытие вклада.", true);
+		} else {
+			addRecord(list, endDate, depositAmount, lowInterestRate, "Досрочное закрытие вклада.", true);
 		}
 
 		return new DepositAccount(this, list);
