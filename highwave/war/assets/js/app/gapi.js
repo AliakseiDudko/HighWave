7be2b2453f63c781aafac54e75e92f59344b94e7
aleksei.dudko <aleksei.dudko@gmail.com>
2014-09-28 13:43:38 +0300

    Issue with API was fixed.

--- highwave/war/assets/js/app/gapi.js
+++ highwave/war/assets/js/app/gapi.js
@@ -1,30 +1,33 @@
 define([ "backbone" ], function(Backbone) {
     var ApiManager = {};
 
+    onLoadGapi = function() {
+        Backbone.Events.trigger("app:load");
+    };
+
+    init = function() {
+        // var root = "http://localhost:8888/_ah/api";
+        var root = "https://high-wave-595.appspot.com/_ah/api";
+
+        gapi.client.load("deposits", "v0", onLoadGapi, root);
+    };
+
+    checkGapi = function checkGapi() {
+        if (gapi && gapi.client) {
+            init();
+        } else {
+            setTimeout(checkGapi, 10);
+        }
+    };
+
     ApiManager.loadGapi = function() {
         var self = this;
 
         if (typeof gapi !== "undefined") {
-            return this.init();
+            return init();
         }
 
-        require([ "https://apis.google.com/js/client.js?onload=define" ], function() {
-            function checkGapi() {
-                if (gapi && gapi.client) {
-                    self.init();
-                } else {
-                    setTimeout(checkGapi, 10);
-                }
-            }
-
-            checkGapi();
-        });
-    };
-
-    ApiManager.init = function() {
-        gapi.client.load("deposits", "v0", function() {
-            Backbone.Events.trigger("app:load");
-        }, "http://high-wave-595.com/_ah/api");
+        require([ "https://apis.google.com/js/client.js?onload=define" ], checkGapi);
     };
 
     return ApiManager;
