bd44067ae63aa346752a532f788323becb2d1dc0
aleksei.dudko <aleksei.dudko@gmail.com>
2014-11-08 18:04:06 +0300

    Catch Moment deposit was reviewed.

--- highwave/src/com/dudko/highwave/deposit/deposits/CatchMomentDeposit.java
+++ highwave/src/com/dudko/highwave/deposit/deposits/CatchMomentDeposit.java
@@ -25,16 +25,15 @@ public class CatchMomentDeposit extends Deposit {
 			return null;
 		}
 
-		List<AccountStatementRecord> list = new ArrayList<AccountStatementRecord>();
-
-		int depositTerm = 732;
-		int term = Math.min(period, depositTerm);
+		int maxTerm = 732;
+		int term = Math.min(period, maxTerm);
 		DateTime currentDate = DateTime.now();
 		DateTime endDate = currentDate.plusDays(term);
 
 		float _interestRate = interestRate(term);
 		float _amount = amount;
 
+		List<AccountStatementRecord> list = new ArrayList<AccountStatementRecord>();
 		addRecord(list, currentDate, _amount, interestRate, RecordDescriptions.MSG_000_Open_Deposit);
 
 		DateTime previousDate = currentDate;
@@ -54,7 +53,11 @@ public class CatchMomentDeposit extends Deposit {
 			addRecord(list, endDate, _amount, _interestRate, RecordDescriptions.MSG_002_Accrual_Of_Interest);
 		}
 
-		addRecord(list, endDate, _amount, _interestRate, RecordDescriptions.MSG_003_Close_Deposit, true);
+		if (term == maxTerm) {
+			addRecord(list, endDate, _amount, _interestRate, RecordDescriptions.MSG_003_Close_Deposit, true);
+		} else {
+			addRecord(list, endDate, _amount, _interestRate, RecordDescriptions.MSG_005_Early_Withdrawal_Of_Deposit, true);
+		}
 
 		return new DepositAccount(this, list);
 	}
